54f6c7d99066d84f527834aacefa03d12526e680
Messi <1157875619@qq.com>
2026-02-23 23:00:48 +0800

    测试1

--- src/main/java/com/example/springbootmybatis/entity/Address.java
+++ src/main/java/com/example/springbootmybatis/entity/Address.java
@@ -7,10 +7,11 @@ import lombok.Data;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Address {
+    
     @JsonProperty("street")
     private String street;
     @JsonProperty("city")
     private String city;
     @JsonProperty("zipCode")
-    private String zipCode;
+    private String zipCode; 
 }
